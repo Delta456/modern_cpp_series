10cbc98ca203c976d443ae9a92d7666b65832b9f
Swastik Baranwal <swstkbaranwal@gmail.com>
2020-03-25 12:21:17 +0530

    raw_string: more

---
+++ raw_string/new.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+std::string dir = R"(C:\User\Home\Folder)";
+std::string special_str = R"(newline\n)";
+
+std::cout << dir << std::endl;
+std::cout << special_str << std::endl;
+
+ return 0;
+}    
