b066d8e6b8d3d196caaf953cf0e537dd0acbb798
Swastik Baranwal <swstkbaranwal@gmail.com>
2020-03-25 12:19:15 +0530

    raw_string: init commit

---
+++ raw_string/traditional.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+std::string dir = "C:\User\Home\Folder";  
+std::string special_str = "newline\n"; 
+
+std::cout << dir << std::endl;
+std::cout << special_str << std::endl;
+
+ return 0;
+}
