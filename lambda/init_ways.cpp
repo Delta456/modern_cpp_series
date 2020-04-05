8a6e9fdf8b8488c5ff639c11b1ab2009fd0daaf3
Swastik Baranwal <swstkbaranwal@gmail.com>
2020-04-05 19:20:50 +0530

    init_ways: fix braces

--- lambda/init_ways.cpp
+++ lambda/init_ways.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 
 int main() {
+ int add = [](int a){ return 2 + 3; }(); // using lambda then storing the value
 
-int add = [](int a){ return 2 + 3; }(); // using lambda then storing the value
+ [](){ std::cout<< "Caliing like this also works" << std::endl; }(); 
 
-[](){ std::cout<< "Caliing like this also works" << std::endl; }(); 
-
-auto callback = [](auto a, auto b) { 
+ auto callback = [](auto a, auto b) { 
         a == b; 
     }; // using a label and then using it for various purposes
-
 }
