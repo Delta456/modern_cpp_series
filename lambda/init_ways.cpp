437f738b772022550f9b5b7d13e0154b466e0111
Swastik Baranwal <swstkbaranwal@gmail.com>
2020-04-05 19:13:03 +0530

    lambda: init commit

---
+++ lambda/init_ways.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+int add = [](int a){ return 2 + 3; }(); // using lambda then storing the value
+
+[](){ std::cout<< "Caliing like this also works" << std::endl; }(); 
+
+auto callback = [](auto a, auto b) { 
+        a == b; 
+    }; // using a label and then using it for various purposes
+
+}
